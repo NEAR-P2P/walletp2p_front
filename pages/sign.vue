33d8daebb24870a5086f42e3f779cc8b45b88ead
Hector Palencia <hrpmicarelli@gmail.com>
2024-04-01 15:44:07 -0400

    pruebas

--- pages/sign.vue
+++ pages/sign.vue
@@ -153,6 +153,10 @@
 import moment from 'moment';
 // import {functionCall} from 'near-api-js/lib/transaction'
 import BN from 'bn.js';
+import * as nearAPI from "near-api-js";
+import { baseDecode } from 'near-api-js/lib/utils/serialize';
+import { PublicKey } from 'near-api-js/lib/utils';
+import { createTransaction /* , functionCall */ } from 'near-api-js/lib/transaction'
 import utils from '../services/utils';
 import encryp from '../services/encryp';
 import localStorageUser from '~/services/local-storage-user';
@@ -161,6 +165,9 @@ import { configNear }  from '~/services/nearConfig';
 
 import walletUtils from '@/services/wallet';
 import { ALERT_TYPE } from '~/plugins/dictionary';
+
+// const nearAPI = require('near-api-js');
+
 
 // const nearAPI = require("near-api-js");
 // const { KeyPair, keyStores, connect, WalletConnection, transactions, Account } = nearAPI;
@@ -253,6 +260,190 @@ export default {
       }
     },
 
+    async approved2() {
+      try {
+        // this.loading = true
+        if(Number(this.balance) < Number(this.attachedDeposit)) {
+          this.loading = false
+          this.$alert(ALERT_TYPE.ERROR, { desc: "Su balance no es suficiente" })
+          return
+        }
+        // const token = JSON.parse(sessionStorage.getItem("token"));
+        const dataUser = localStorageUser.getAccount(this.token.from);
+        const privateKey = dataUser.privateKey;
+        const address = dataUser.address;
+        
+
+        
+        const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+        const keyPairNew = nearAPI.KeyPair.fromString(privateKey);
+        await keyStore.setKey(process.env.Network, address, keyPairNew);
+        const near = await nearAPI.connect(configNear(keyStore));
+        // const account = await near.account(address);
+
+        /* let response
+        if(!Array.isArray(this.token?.json)) {
+          response = await account.functionCall(this.token.json);
+        } else {
+          const actions = this.token?.json.map((item) => {
+              return nearAPI.transactions.functionCall(
+                item.methodName, 
+                !item?.args ? {} : item.args,
+                new BN((!item?.gas ? '3000000000000' : item.gas)),
+                new BN((!item?.attachedDeposit ? "0" : item.attachedDeposit))
+              )
+            });
+
+          response = await account.signAndSendTransaction({ receiverId: this.token?.json[0]?.contractId, actions});
+
+        } */
+        
+        
+        // near call usdt.tokens-pruebas.testnet storage_deposit '{"account_id":"daov5.metademocracia.testnet"}' --accountId hpalencia.testnet --amount 0.00125 
+        
+
+        const txs = [];
+
+        txs.push({
+          receiverId: "usdt.tokens-pruebas.testnet",
+          functionCalls: [
+            {
+              methodName: "storage_deposit",
+              args: {
+                "account_id": "andromeda2018.testnet"
+              },
+              gas: "300000000000000",
+              deposit: "1250000000000000000000"
+            }
+          ]
+        });
+
+        txs.push({
+          receiverId: "usdc.tokens-pruebas.testnet",
+          functionCalls: [
+            {
+              methodName: "storage_deposit",
+              args: {
+                "account_id": "andromeda2018.testnet"
+              },
+              gas: "300000000000000",
+              deposit: "1250000000000000000000"
+            }
+          ]
+        });
+
+        /* const near = await nearAPI.connect(
+					CONFIG(new keyStores.BrowserLocalStorageKeyStore(), localStorage.getItem("wallet"))
+				); */
+				const wallet = new  nearAPI.WalletConnection(near);
+				const nearTransactions = await Promise.all(
+					txs.map(async tx => {
+						return await this.createTransactionFn(
+							tx.receiverId,
+							tx.functionCalls.map(fc => {
+                console.log("argumentos: ", fc.args)
+								return nearAPI.transactions.functionCall(fc.methodName, fc.args, new BN((!fc?.gas ? '3000000000000' : fc.gas)),
+                new BN((!fc?.deposit ? "0" : fc.deposit)));
+							})
+						);
+					})
+				);
+          
+        console.log(nearTransactions)
+
+        const result = await wallet.requestSignTransactions({
+					transactions: nearTransactions,
+					// callbackUrl: options?.callbackUrl,
+					// meta: options?.meta
+				});
+
+        console.log("///////////////////////////////")
+        console.log(result)
+        console.log("///////////////////////////////")
+        
+        
+      }
+      catch (error) {
+        this.loading = false
+        this.$alert(ALERT_TYPE.ERROR, { desc: error.toString() })
+        // console.log("error error: ", error.toString());
+      }
+    },
+
+    async createTransactionFn(
+      receiverId,
+      actions
+    ){
+      console.log("paso 1")
+      const dataUser = localStorageUser.getAccount(this.token.from);
+      const privateKey = dataUser.privateKey;
+      const address = dataUser.address;
+      
+
+      
+      const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+      const keyPairNew = nearAPI.KeyPair.fromString(privateKey);
+      const localKey = keyPairNew.getPublicKey();
+      await keyStore.setKey(process.env.Network, address, keyPairNew);
+      const near = await nearAPI.connect(configNear(keyStore));
+      // const account = (await near.account(address)).getAccessKeys;
+
+      // const near = await nearAPI.connect(CONFIG(new keyStores.BrowserLocalStorageKeyStore()));
+      const wallet = new nearAPI.WalletConnection(near);
+
+      if (!wallet || !near) {
+        throw new Error(`No active wallet or NEAR connection.`)
+      }
+      console.log("paso 2 ", wallet.getAccountId(), address, receiverId,  actions, wallet.account(), wallet.account().accountId)
+      /* const localKey =
+        await near.connection.signer.getPublicKey(
+          address, // wallet?.account().accountId,
+          near.connection.networkId
+        ) */
+        console.log("paso 3 ", localKey)
+      const accessKey = await wallet
+        .account()
+        .accessKeyForTransaction(receiverId, actions, localKey)// .catch((error) => {console.log("error: ", error)})
+
+      if (!accessKey) {
+        throw new Error(
+          `Cannot find matching key for transaction sent to ${receiverId}`
+        )
+      }
+      console.log("paso 4")
+      
+
+      const block = await near?.connection.provider.block({
+        finality: 'final',
+      })
+
+      if (!block) {
+        throw new Error(`Cannot find block for transaction sent to ${receiverId}`)
+      }
+
+      console.log("paso 5")
+
+      const blockHash = baseDecode(block?.header?.hash)
+      // const blockHash = nearAPI.utils.serialize.base_decode(accessKey.block_hash);
+      console.log("paso 6")
+      const publicKey = PublicKey.from(accessKey.public_key)
+      // const nonce = accessKey.access_key.nonce + nonceOffset
+      console.log("paso 7")
+      const nonce = ++accessKey.access_key.nonce;
+      console.log("paso 8 ", wallet?.account().accountId)
+
+      
+
+      return createTransaction(
+        address, // wallet?.account().accountId,
+        publicKey,
+        receiverId,
+        nonce,
+        actions,
+        blockHash
+      )
+    },
+
     async approved() {
       try {
         this.loading = true
@@ -273,7 +464,7 @@ export default {
         await keyStore.setKey(process.env.Network, address, keyPairNew);
         const near = await nearAPI.connect(configNear(keyStore));
         const account = await near.account(address);
-
+        
         let response
         if(!Array.isArray(this.token?.json)) {
           response = await account.functionCall(this.token.json);
@@ -346,6 +537,226 @@ export default {
   },
     
 }
+
+/*
+async initContract() {
+				//Verifying if the user has token activo
+				let txs = [];
+				const CONTRACT_NAME = process.env.VUE_APP_CONTRACT_NAME;
+				const CONTRACT_NAME_USDT = process.env.VUE_APP_CONTRACT_NAME_USDT;
+				const near = await connect(
+					CONFIG(new keyStores.BrowserLocalStorageKeyStore(), localStorage.getItem("wallet"))
+				);
+				const wallet = new WalletConnection(near);
+				const contract = new Contract(wallet.account(), CONTRACT_NAME, {
+					viewMethods: ["get_token_activo"],
+					changeMethods: [""],
+					sender: wallet.account()
+				});
+				//Info to set mail address
+				this.$apollo
+					.watchQuery({
+						query: mails,
+						variables: {
+							owner_id: this.$session.get("owner_id")
+						}
+					})
+					.subscribe(({data}) => {
+						this.$session.set(
+							"user_mail",
+							data.datausers[0].email === null
+								? "xxx"
+								: data.datausers[0].email
+						);
+					});
+				// eslint-disable-next-line no-unused-vars
+				var vldeposit = "100000000000000000000000";
+				var get_token_activo = await contract.get_token_activo({
+					user_id: this.typeoffer == "sell" ? this.userInfo.split(".")[0] + "." + CONTRACT_NAME : this.userInfo,
+					ft_token: "USDT"
+				});
+				if (get_token_activo == true) {
+					vldeposit = "1";
+				} else {
+					vldeposit = "100000000000000000000000";
+					txs.push({
+						receiverId: CONTRACT_NAME,
+						functionCalls: [
+							{
+								methodName: "activar_subcuenta_ft",
+								receiverId: CONTRACT_NAME,
+								gas: "80000000000000",
+								args: {subaccount_id: this.typeoffer == "sell" ? this.userInfo.split(".")[0] + "." + CONTRACT_NAME : this.userInfo, asset: "USDT"},
+								deposit: "100000000000000000000000"
+							}
+							]
+						});
+				}
+				var now = moment()
+					.format("YYYY-MM-DD HH:mm:ss")
+					.toString();
+				if (this.typeoffer == "sell" && this.subcontract == false) {
+					txs.push(
+						{
+							//Deploy contract
+							receiverId: CONTRACT_NAME,
+							functionCalls: [
+								{
+									methodName: "create_subcontract_user",
+									receiverId: CONTRACT_NAME,
+									gas: "80000000000000",
+									args: {},
+									deposit: vldeposit
+								}
+							]
+						});
+						txs.push({
+							//Send Near
+							receiverId: CONTRACT_NAME_USDT,
+							functionCalls: [
+								{
+									methodName: "ft_transfer",
+									receiverId: CONTRACT_NAME_USDT,
+									gas: "80000000000000",
+									args: {
+										receiver_id: this.userInfo.split(".")[0] + "." + CONTRACT_NAME,
+											//this.userInfo.split(".")[0] + "." + CONTRACT_NAME,
+											
+										amount: this.$session.get("totalremaining_amount").toString()
+									},
+									deposit: 1
+								}
+							]
+						});
+						txs.push({
+							//Accept Order
+							receiverId: CONTRACT_NAME,
+							functionCalls: [
+								{
+									methodName: "accept_offer",
+									receiverId: CONTRACT_NAME,
+									gas: "300000000000000",
+									args: {
+										offer_type: parseInt(this.$session.get("offer_type")),
+										offer_id: parseInt(this.$session.get("offer_id")),
+										amount: this.$session.get("totalremaining_amount").toString(),
+										payment_method: parseInt(
+											this.$session.get("payment_method")
+										),
+										datetime: now,
+										rate: parseFloat(this.$session.get("price"))
+									},
+									deposit: "1"
+								}
+							]
+						});
+					
+				} 
+				else if (this.typeoffer == "sell" && this.subcontract == true) 
+				{
+					txs.push(
+						{
+							//Send Near
+							receiverId: CONTRACT_NAME_USDT,
+							functionCalls: [
+								{
+									methodName: "ft_transfer",
+									receiverId: CONTRACT_NAME_USDT,
+									gas: "80000000000000",
+									args: {
+										receiver_id:
+											this.userInfo.split(".")[0]+ "." + CONTRACT_NAME,
+										amount: this.$session.get("totalremaining_amount").toString()
+									},
+									deposit: "1"
+								}
+							]
+						});
+						txs.push({
+							//Accept Order
+							receiverId: CONTRACT_NAME,
+							functionCalls: [
+								{
+									methodName: "accept_offer",
+									receiverId: CONTRACT_NAME,
+									gas: "300000000000000",
+									args: {
+										offer_type: parseInt(this.$session.get("offer_type")),
+										offer_id: parseInt(this.$session.get("offer_id")),
+										amount: this.$session.get("totalremaining_amount").toString(),
+										payment_method: parseInt(
+											this.$session.get("payment_method")
+										),
+										datetime: now,
+										rate: parseFloat(this.$session.get("price"))
+									},
+									deposit: "1"
+								}
+							]
+						});
+					
+				} 
+				else 
+				{
+					txs.push(
+						{
+							//Accept Order
+							receiverId: CONTRACT_NAME,
+							functionCalls: [
+								{
+									methodName: "accept_offer",
+									receiverId: CONTRACT_NAME,
+									gas: "300000000000000",
+									args: {
+										offer_type: parseInt(this.$session.get("offer_type")),
+										offer_id: parseInt(this.$session.get("offer_id")),
+										amount: this.$session.get("totalremaining_amount").toString(),
+										payment_method: parseInt(
+											this.$session.get("payment_method")
+										),
+										datetime: now,
+										rate: parseFloat(this.$session.get("price"))
+									},
+									deposit: "1"
+								}
+							]
+						});
+				}
+				//console.log(txs)
+				this.batchTransaction(txs, { meta: "USDT" });
+			}
+		}
+	};
+
+  //Batch transactions
+			async batchTransaction(transactions, options) {
+				const near = await connect(
+					CONFIG(new keyStores.BrowserLocalStorageKeyStore(), localStorage.getItem("wallet"))
+				);
+				const wallet = new WalletConnection(near);
+				const nearTransactions = await Promise.all(
+					transactions.map(async tx => {
+						return await this.createTransactionFn(
+							tx.receiverId,
+							tx.functionCalls.map(fc => {
+								return functionCall(fc.methodName, fc.args, fc.gas, fc.deposit);
+							})
+						);
+					})
+				);
+				wallet.requestSignTransactions({
+					transactions: nearTransactions,
+					callbackUrl: options?.callbackUrl,
+					meta: options?.meta
+				});
+			},
+*/
+
 </script>
 
 <style src="@/assets/styles/pages/approve-transaction.scss" lang="scss"></style>
+
+
+
+
+
