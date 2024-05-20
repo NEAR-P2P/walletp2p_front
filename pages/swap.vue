a39ec54bca8f7e9c5fe92906484176c8d8cd1456
dvconsultores <adominguez@dvconsultores.com>
2024-05-20 14:47:38 -0400

    Remove commented out code and update npm dependencies

--- pages/swap.vue
+++ pages/swap.vue
@@ -55,7 +55,7 @@
 
     <Header show-append></Header>
 
-    <img src="@/assets/sources/logos/logotype.svg" alt="logo icon" class="mx-auto mt-1" style="width: 290px">
+    <!-- <img src="@/assets/sources/logos/logotype.svg" alt="logo icon" class="mx-auto mt-1" style="width: 290px"> -->
 
     <section class="d-flex flex-column" style="height: 208px; translate: 0 -30px">
       <v-text-field
