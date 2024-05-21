a8b9e8e81e0affecf7e8262d9a135b26ba88d5b5
dvconsultores <adominguez@dvconsultores.com>
2024-05-21 11:35:01 -0400

    chore: Update Header component in swap.vue

--- pages/swap.vue
+++ pages/swap.vue
@@ -52,10 +52,10 @@
         </div>
       </v-card>
     </v-dialog>
-
-    <Header show-append></Header>
-
-    <!-- <img src="@/assets/sources/logos/logotype.svg" alt="logo icon" class="mx-auto mt-1" style="width: 290px"> -->
+    
+    <Header 
+      show-append
+      style="margin-bottom: 70px;"></Header>
 
     <section class="d-flex flex-column" style="height: 208px; translate: 0 -30px">
       <v-text-field
